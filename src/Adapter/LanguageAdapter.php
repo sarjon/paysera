b3422f48585de5a32839acc6b200a066bb7c89a8
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2017-05-12 22:06:45 +0300

    updated name

--- src/Adapter/LanguageAdapter.php
+++ src/Adapter/LanguageAdapter.php
@@ -2,8 +2,8 @@
 /**
  * This file is part of the paysera module.
  *
- * @author    Sarunas Jonusas, https://github.com/sarjon
- * @copyright Copyright (c) Sarunas Jonusas
+ * @author    Šarūnas Jonušas, https://github.com/sarjon
+ * @copyright Copyright (c) Šarūnas Jonušas
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- views/js/front/payment-methods.js
+++ views/js/front/payment-methods.js
@@ -1,8 +1,8 @@
 /**
  * This file is part of the paysera module.
  *
- * @author    Sarunas Jonusas, https://github.com/sarjon
- * @copyright Copyright (c) Sarunas Jonusas
+ * @author    Šarūnas Jonušas, https://github.com/sarjon
+ * @copyright Copyright (c) Šarūnas Jonušas
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
