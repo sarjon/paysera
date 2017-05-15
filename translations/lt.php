903e40f5cbff6a21ff2a92c0d6462633e1eda246
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2017-05-15 23:32:39 +0300

    added missing lithuanian translations

--- translations/lt.php
+++ translations/lt.php
@@ -14,8 +14,16 @@ $_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_a5f555f0e1684
 $_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_ac33dbbd1f5409ed26d864a53fb6d598'] = 'Numatyta mokėjimo šalis';
 $_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_f0fffd3435eaafbcc1a740bb79de1e9d'] = 'Testavimo rėžimas';
 $_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_c9cc8cce247e49bae79f15173ce97354'] = 'Išsaugoti';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_2a7bfe2453a8dfee2881b578819d1fc9'] = 'Svetainės nuosavybės patikrinimas';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_544632710cfd98c710d272f3c6214f5a'] = 'Pridėti nuosavybės turinį';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_943769b205da24c9b0fe9808370b97b1'] = 'Nuosavybės patvirtinimo kodas';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_c82760d98247a4fa0b6266de3b5b20d7'] = 'Jūsų puslapio nuosavybės patvirtinimo kodas';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_241683f6cb80f41387c9356fb5a3100d'] = 'Pavyzdinis nuosavybės patvirtinimo kodas: %s';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_5f8cf14f385c3de02e142214ae9c434d'] = 'Paysera papildoma konfigūracija';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_dcb694c2c7d4f08d42c8d115b5d611fb'] = 'Rodyti Paysera kokybės ženklą';
+$_MODULE['<{paysera}prestashop>adminpayseraconfigurationcontroller_369a82d4f81dbb44f8b27d5a773c3217'] = 'Rodyti Paysera kokybės ženklą svetainės puslapiuose';
 $_MODULE['<{paysera}prestashop>cancel_444bd3641b9dc3a440e2ec45be689dc7'] = 'Mokėjimas atšauktas';
 $_MODULE['<{paysera}prestashop>validation_e2b7dec8fa4b498156dfee6e4c84b156'] = 'Šis mokėjimo būdas nėra galimas';
+$_MODULE['<{paysera}prestashop>display-order-detail_6d952efb799b6d822438bc3f8f521e68'] = 'Pereiti prie apmokėjimo naudojant Paysera';
 $_MODULE['<{paysera}prestashop>payment-options_48034ce5085e9fd93c65ce59966acf9e'] = 'Pasirinkite mokėjimo šalį';
 $_MODULE['<{paysera}prestashop>payment-options_cdfc09d94f2116216fc3aae9cfa2c095'] = 'Mokėjimo šalis';
-$_MODULE['<{paysera}prestashop>display-order-detail_6d952efb799b6d822438bc3f8f521e68'] = 'Pereiti prie apmokėjimo naudojant Paysera';
